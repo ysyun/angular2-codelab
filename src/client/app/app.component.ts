c9b9802ec283f199bd76fe96ec091797e873b3f3
Peter Yun <nulpulum@gmail.com>
2016-07-08 17:27:01 +0900

    chore(remove): remove uneccessary files

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { HTTP_PROVIDERS } from '@angular/http';
 
-import { Config, NameListService, NavbarComponent, ToolbarComponent } from './shared/index';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -10,13 +8,10 @@ import { Config, NameListService, NavbarComponent, ToolbarComponent } from './sh
  */
 @Component({
   moduleId: module.id,
-  selector: 'sd-app',
-  viewProviders: [NameListService, HTTP_PROVIDERS],
+  selector: 'a3-app',
   templateUrl: 'app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
 })
 export class AppComponent {
   constructor() {
-    console.log('Environment config', Config);
   }
 }
