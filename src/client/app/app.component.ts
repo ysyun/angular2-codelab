989ff4ed5cbad21e5785280f66728bd6ecd21909
Peter Yun <nulpulum@gmail.com>
2016-07-13 16:39:53 +0900

    feat(rxjs): add rxjs create sample

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CreateTest } from './rxjs-codelab/create';
 
 @Component({
     selector: 'a3-app',
     template: `
         <div> A3 Application</div>
-    `
+    `,
+    providers: [CreateTest]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     
+    constructor(
+        private _createTest: CreateTest
+    ) {}
+
+    ngOnInit() {
+        this._createTest.creat01();
+    }
 }
---
+++ src/client/app/rxjs-codelab/create.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class CreateTest {
+    
+    creat01() {
+        let source = Observable.create(function (observer: any) {
+            observer.next(42);
+            observer.complete();
+
+            // Note that this is optional, you do not have to return this if you require no cleanup
+            return () => { console.log('disposed'); };
+        });
+
+        let subscription = source.subscribe(
+            (x: any) => { console.log('Next: ' + x); },
+            (err: any) => { console.log('Error: ' + err); },
+            () => { console.log('Completed'); });        
+    }
+
+}
